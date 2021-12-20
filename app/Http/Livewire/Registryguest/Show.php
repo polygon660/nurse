779e711d5c3390046bdbe99895e1ec80dc787bba
polygon660 <polygon660@users.noreply.github.com>
2021-12-20 19:53:08 +0700

    update

--- app/Http/Livewire/Registryguest/Show.php
+++ app/Http/Livewire/Registryguest/Show.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Registryguest;
 use App\Models\gender;
 use App\Models\guest;
 use App\Models\guest_type;
+use App\Models\history;
 use App\Models\level;
 use App\Models\prefix;
 use App\Models\program;
@@ -15,23 +16,23 @@ class Show extends Component
 {
     protected $listeners = ['delete'];
 
-    public $guest_id, $input_type, $input_code, $input_prefix, $input_name, $input_surname, $input_gender,
-        $input_level, $input_room, $input_program, $input_weight, $input_height;
+    public $guest_id, $guest_type, $code, $prefix, $name, $surname, $gender,
+        $level, $room, $program, $weight, $height;
 
     public $updateMode = false;
 
     protected $rules = [
-        'input_type' => 'required',
-        'input_code' => '',
-        'input_prefix' => 'required',
-        'input_name' => 'required',
-        'input_surname' => 'required',
-        'input_gender' => 'required',
-        'input_level' => '',
-        'input_room' => '',
-        'input_program' => '',
-        'input_weight' => '',
-        'input_height' => '',
+        'guest_type' => 'required',
+        'code' => '',
+        'prefix' => 'required',
+        'name' => 'required',
+        'surname' => 'required',
+        'gender' => 'required',
+        'level' => '',
+        'room' => '',
+        'program' => '',
+        'weight' => '',
+        'height' => '',
     ];
 
     public function edit($id)
@@ -39,17 +40,17 @@ class Show extends Component
         $this->updateMode = true;
         $item = guest::findOrFail($id);
         $this->guest_id = $item->id;
-        $this->input_type = $item->guest_type_id;
-        $this->input_code = $item->code;
-        $this->input_prefix = $item->prefix_id;
-        $this->input_name = $item->name;
-        $this->input_surname = $item->surname;
-        $this->input_gender = $item->gender_id;
-        $this->input_level = $item->level_id;
-        $this->input_room = $item->room_id;
-        $this->input_program = $item->program_id;
-        $this->input_weight = $item->weight;
-        $this->input_height = $item->height;
+        $this->guest_type = $item->guest_type_id;
+        $this->code = $item->code;
+        $this->prefix = $item->prefix_id;
+        $this->name = $item->name;
+        $this->surname = $item->surname;
+        $this->gender = $item->gender_id;
+        $this->level = $item->level_id;
+        $this->room = $item->room_id;
+        $this->program = $item->program_id;
+        $this->weight = $item->weight;
+        $this->height = $item->height;
     }
 
     public function save()
@@ -60,17 +61,17 @@ class Show extends Component
         try {
             $guest = guest::findOrFail($this->guest_id);
             $guest->update([
-                'guest_type_id' => $this->input_type,
-                'code' => $this->input_code,
-                'prefix_id' =>  $this->input_prefix,
-                'name' =>  $this->input_name,
-                'surname' => $this->input_surname,
-                'gender_id' => $this->input_gender,
-                'level_id' => $this->input_level,
-                'room_id' => $this->input_room,
-                'program_id' => $this->input_program,
-                'weight' => $this->input_weight,
-                'height' => $this->input_height,
+                'guest_type_id' => $this->type,
+                'code' => $this->code,
+                'prefix_id' =>  $this->prefix,
+                'name' =>  $this->name,
+                'surname' => $this->surname,
+                'gender_id' => $this->gender,
+                'level_id' => $this->level,
+                'room_id' => $this->room,
+                'program_id' => $this->program,
+                'weight' => $this->weight,
+                'height' => $this->height,
             ]);
             $this->reset();
             // $this->dispatchBrowserEvent('swal:modal', [
@@ -115,6 +116,7 @@ class Show extends Component
     public function render()
     {
         $data = guest::with(['guest_type','prefix','level','room','program'])->get();
+        $datahistry = history::where('guest_id',$this->guest_id)->get();
         $type = guest_type::all();
         $prefix = prefix::all();
         $level = level::all();
@@ -124,6 +126,7 @@ class Show extends Component
 
         return view('livewire.registryguest.show')->with([
             'data' => $data,
+            'datahistry' => $datahistry,
             'type' => $type,
             'prefix' => $prefix,
             'gender' => $gender,
